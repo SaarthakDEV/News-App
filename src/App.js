a84edcfc2898e6427d068278b9408a0c1ac7d59b
SaarthakDEV <91599391+SaarthakGitHub@users.noreply.github.com>
2025-02-20 00:36:44 +0530

    Refactor News component to use HashRouter and update country parameter; remove unused files and dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import Navbar from "./Component/Navbar";
 import News from './Component/News';
 import{
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
@@ -16,14 +16,14 @@ export default class App extends Component {
         <Navbar />
         {/* <News pageSize={this.pageSize} country="in" category="sports"/> */}
         <Routes>
-        <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general"/>} />
-        <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports"/>} />
-        <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>} />
-        <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general"/>} />
-        <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business"/>} />
-        <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science"/>} />
-        <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology"/>} />
-        <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health"/>} />
+        <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="us" category="general"/>} />
+        <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="us" category="sports"/>} />
+        <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>} />
+        <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="us" category="general"/>} />
+        <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="us" category="business"/>} />
+        <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="us" category="science"/>} />
+        <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="us" category="technology"/>} />
+        <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="us" category="health"/>} />
         </Routes>
       </Router>
         
