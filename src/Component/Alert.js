20a456cc4fbd95a22e7adfbd5fdd7aef12d14273
SaarthakDEV <91599391+SaarthakGitHub@users.noreply.github.com>
2025-02-20 23:06:10 +0530

    Added bootstrap javascript cdn

--- src/Component/Alert.js
+++ src/Component/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alert = () => {
     return (
-        <div class="alert alert-danger" role="alert" style={{marginTop: "7vh"}}>
+        <div className="alert alert-danger" role="alert" style={{marginTop: "7vh"}}>
             You are viewing static data because the API is encountering a CORS error in production mode
         </div>
     )
